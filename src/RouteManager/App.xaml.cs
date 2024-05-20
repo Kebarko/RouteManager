9fe3f4762396bf4685dd4e01a95166f20211fe50
Miroslav Rábek <rabek.miroslav@gmail.com>
2024-05-20 16:31:03 +0200

    File-scoped namespaces.

--- src/RouteManager/App.xaml.cs
+++ src/RouteManager/App.xaml.cs
@@ -1,33 +1,32 @@
 ﻿using Serilog;
 using System.Windows;
 
-namespace KE.MSTS.RouteManager
+namespace KE.MSTS.RouteManager;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
+        base.OnStartup(e);
 
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.File(
-                    path: "logs/RouteManager.log",
-                    rollingInterval: RollingInterval.Day,
-                    rollOnFileSizeLimit: false,
-                    retainedFileCountLimit: 3,
-                    shared: true,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
-                .CreateLogger();
-        }
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(
+                path: "logs/RouteManager.log",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: false,
+                retainedFileCountLimit: 3,
+                shared: true,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+    }
 
-        protected override void OnExit(ExitEventArgs e)
-        {
-            base.OnExit(e);
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
 
-            Log.CloseAndFlush();
-        }
+        Log.CloseAndFlush();
     }
 }
